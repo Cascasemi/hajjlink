3123877efb460e20df87566b2aeb37c70f849e01
unknown <kwameroland5@gmail.com>
2025-10-16 04:53:04 +0000

    home screen1

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'dart:math' as math;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
